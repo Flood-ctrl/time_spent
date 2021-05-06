8c005b46671387f96a64f6acbd3d813e7a92b074
Serhii Novokhatskyi <serhiinovokhatskyi@Serhiis-MacBook-Air.local>
2021-05-06 20:54:15 +0300

    Add colors

--- time_spent.py
+++ time_spent.py
@@ -3,6 +3,17 @@
 import time
 import sys
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # def spentTime(input_function):
 #     start_time = time.time()
 #     input_function()
@@ -23,6 +34,6 @@ def sleepFucn():
         spent_time = spent_time - start_time
         spent_time = time.strftime('%H:%M:%S', time.gmtime(spent_time))
         function_name = sys._getframe().f_code.co_name
-        print (f"Time spent in {function_name} : {spent_time}")
+        print (f"{bcolors.WARNING}Time spent in {function_name} : {spent_time}")
 
 sleepFucn()
